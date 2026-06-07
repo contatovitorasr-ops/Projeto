fa0928b0af0e435205673d70dacbd824c0c3cb5f
Vítor Adriano <contato.vitorasr@gmail.com>
2026-06-07 13:59:44 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -927,7 +927,7 @@ const aulas = [
     {id:"aula40", materia:"Revisão Final", titulo:"Revisão de Raciocínio Lógico", video:"https://www.youtube-nocookie.com/embed/T3Y83n6B2Yg", pdf:"pdfs/revisao_logico.pdf", mapa:"mapas/revisao_logico.png"},
     {id:"aula41", materia:"Revisão Final", titulo:"Revisão de Direito", video:"https://www.youtube-nocookie.com/embed/zvprImSjg3g", pdf:"pdfs/revisao_03.pdf", mapa:"mapas/revisao_03.png"},
     {id:"aula42", materia:"Revisão Final", titulo:"Revisão de Sistema Prisional", video:"", pdf:"pdfs/revisao_04.pdf", mapa:"mapas/revisao_04.png"},
-    {id:"aula43", materia:"Revisão Final", titulo:"Simulado Geral Comentado", video:"", pdf:"pdfs/revisao_05.pdf", mapa:"mapas/revisao_05.png"},
+    {id:"aula43", materia:"Revisão Final", titulo:"Simulado Geral Comentado", video:"", pdf:"pdfs/revisao_05.pdf", mapa:"mapas/revisao_05.png"}
     
 
 ];
